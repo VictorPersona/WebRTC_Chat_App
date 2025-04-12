70a52912a9e0c5f9b8cca8b40bbc298b2c3e504d
VictorPersona <gauravtupe00@gmail.com>
2025-04-12 09:34:59 +0530

    add initial React components and setup with Vite

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Typography,AppBar } from '@mui/material'
+import VideoPlayer from './components/VideoPlayer'
+import Options from './components/Options'
+import Notifications from './components/Notifications'
+
+const App = () => {
+  return (
+    <div>
+        <AppBar position='static' color='inherit'>
+    <Typography variant='h2' align='center'></Typography>
+        </AppBar>
+        <VideoPlayer/>
+        <Options>
+            <Notifications/>
+        </Options>
+    </div>
+  )
+}
+
+export default App
